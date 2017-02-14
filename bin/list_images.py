4b969bec6247081831d954ce01de831b98e2c5ce
James <houstonj1@wit.edu>
2017-02-14 18:19:10 -0500

    Updated list_images and pull_image scripts

--- bin/list_images.py
+++ bin/list_images.py
@@ -1,26 +1,40 @@
+#!/usr/bin/python
 # James Houston
-# Initial list images testing
+# Container Orchestrator
+# list_images.py
 
 import docker
-import requests
+from docker.errors import APIError
 from requests import ConnectionError
 from requests import ConnectTimeout
 
-# Create docker client
-client = docker.from_env()
+def main():
+    # Create the docker client
+    client = docker.from_env()
 
-# List images
-try:
-    imageList = client.images.list()
-# Handle exceptions -- log error and recover
-except ConnectionError:
-    print "Connection error exception thrown"
-except ConnectTimeout:
-    print "Connect timeout exception thrown"
-else: # This means we passed with no exceptions
-    print "Image List: ", imageList
-    if not imageList:
-        print "Image Names:"
-        for image in imageList:
-            nameStr = image.tags[0]
-            print nameStr
+    # Get the image list
+    # Throws docker.errors.APIError if server returns an error
+    # Throws requests.ConnectTimeout if the http request to docker times out
+    # Throws requests.ConnectionError if the docker daemon is unreachable
+    try:
+        imageList = client.images.list()
+    except APIError as e:
+        print "APIError exception thrown! Exception details:"
+        print '\t', e
+    except ConnectTimeout as e:
+        print "ConnectTimeout exception thrown! Exception details:"
+        print '\t', e
+    except ConnectionError as e:
+        print "ConnectionError exception thrown! Exception details:"
+        print '\t', e
+    else:
+        if imageList:
+            print "Image List: ", imageList
+            print "Image Names:"
+            for image in imageList:
+                print image.tags[0]
+        else:
+            print "No images available on local system. <insert suggestion to pull image>"
+
+if __name__ == '__main__':
+    main()
--- bin/pull_image.py
+++ bin/pull_image.py
@@ -1,35 +1,56 @@
 #!/usr/bin/python
 # James Houston
-# Prototype 1
-# Pull image script
+# Container Orchestrator
+# pull_image.py
 
 import docker
 import requests
 import sys
+from docker.errors import APIError
 from requests import ConnectionError
 from requests import ConnectTimeout
 
-# Create docker client
-client = docker.from_env()
-print client
-# get arguments
-argLen = len(sys.argv)
-if argLen < 2:
-    print "Error: Invalid number of arguments. Please provide an correct image name."
-    sys.exit(0)
+def main():
+    # Create docker client
+    client = docker.from_env()
 
-print sys.argv
-imageName = sys.argv[1]
-imageTag = None
-if argLen == 3:
-    imageTag = sys.argv[2]
+    # Get input information -- using CLI arguments for now assuming all args are strings
+    # Image name
+    # Image tag (if applicable)
 
-# Pull image
-if imageTag:
-    #try:
-    pullImage = client.images.pull(imageName + ':' + imageTag)
-    #except:
-#	print "exception"
-else:
-    pullImage = client.images.pull(imageName)
-    print pullImage
+    # Check that minimum arguments are passed
+    print sys.argv
+    argLen = len(sys.argv)
+    print 'length: ', argLen
+    imageTag = 'latest'
+    if argLen < 2:
+        print "Error: Invalid number of arguments. Please provide an correct image name."
+        sys.exit(0)
+    elif argLen > 3:
+        print "Error: Too many arguments. Please provide the correct number of arguments."
+        sys.exit(0)
+    else:
+        imageName = sys.argv[1]
+        # Check for a passed image tag
+        if argLen == 3:
+            imageTag = sys.argv[2]
+
+    # Pull image
+    if imageName:
+        try:
+            pulledImage = client.images.pull(imageName, tag=imageTag)
+        except APIError as e:
+            print "APIError exception thrown! Exception details:"
+            print '\t', e
+        except ConnectTimeout as e:
+            print "ConnectTimeout exception thrown! Exception details:"
+            print '\t', e
+        except ConnectionError as e:
+            print "ConnectionError exception thrown! Exception details:"
+            print '\t', e
+        else:
+            print pulledImage
+            print imageName + " image pulled successfully!"
+
+if __name__ == '__main__':
+    main()
