60b735fadd44d68a0dd76068dcccac6c270d9657
James <houstonj1@wit.edu>
2017-02-26 15:21:51 -0500

    More list information added

--- bin/list_containers.py
+++ bin/list_containers.py
@@ -1,11 +1,11 @@
+#!/usr/bin/python
 # James Houston
-# Prototype 1
-# List container script
+# List containers script
 
 import docker
 import requests
 import sys
-from docker import APIError
+from docker.errors import APIError
 from requests import ConnectionError
 from requests import ConnectTimeout
 
@@ -13,20 +13,35 @@ from requests import ConnectTimeout
 client = docker.from_env()
 
 # get arguments
-#
 argLen = len(sys.argv)
+print argLen
+print sys.argv
 if argLen > 2:
     print "Error: Invalid number of arguments. \'list_containers.py <bool(listAll)>\'"
     sys.exit(0)
 
 # list contianer
-if argLen == 2:
-    # List all
+if argLen == 2:    # List all
     allBool = sys.argv[1]
+    print allBool
     if allBool not in ['true','True','false','False']:
         print "Error: Invalid argument passed. Second argument is of type \'bool\'"
+    else:
+        allBool = bool(allBool)
+        try:
+            containerList = client.containers.list(allBool)
+        except APIError as e:
+            print "APIError exception thrown! Exception details:"
+            print '\t', e
+        except ConnectTimeout as e:
+            print "ConnectTimeout exception thrown! Exception details:"
+            print '\t', e
+        except ConnectionError as e:
+            print "ConnectionError exception thrown! Exception details:"
+            print '\t', e
+else:    # List running
     try:
-        containerList = client.containers.list(bool(allBool))
+        containerList = client.containers.list()
     except APIError as e:
         print "APIError exception thrown! Exception details:"
         print '\t', e
@@ -36,6 +51,11 @@ if argLen == 2:
     except ConnectionError as e:
         print "ConnectionError exception thrown! Exception details:"
         print '\t', e
-else:
-    # List running
-    containerList = client.containers.list()
+
+# Print contianer information
+print containerList
+for container in containerList:
+    shortId = container.short_id
+    name = container.name
+    status = container.status
+    print "Name:", name, "Short ID:", shortId, "Status:", status
