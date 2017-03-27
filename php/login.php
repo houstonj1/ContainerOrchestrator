066f128029dbfffe6f1820982c5d8a446cb70d41
Junju Zhang <zhangj@wit.edu>
2017-03-27 11:38:36 -0400

    zz

---
+++ php/login.php
@@ -0,0 +1,36 @@
+<?php
+	ob_start();
+	if(session_start() != PHP_SESSION_ACTIVE)
+		session_start();
+	
+	require_once('connect.php');
+	
+	//create connection
+	$conn = mysqli_connect(DBHOST,DBUSER,DBPASS,DBNAME);
+	$error = mysqli_connect_errno();
+	
+	if($error != null)
+	{
+		$output = "<p>Unable to connect to database</p>";
+		exit($output);
+	}
+	
+	$user = $_POST['loginusr'];
+	$pass = $_POST['password'];
+	
+	$sql = "SELECT userid FROM account WHERE username = '$user' AND password = '$pass'";
+	$result = $conn->query($sql);
+
+	if($result-> num_rows > 0)
+	{
+		$row = $result -> fetch_assoc();
+		$_SESSION["userid"] = $row["userid"];
+		$_SESSION["username"] = $user;
+		header("Location: ../html/Home.html"); 
+	}
+	else
+	{
+		echo "<script>alert(\"Username or password is incorrect! Try Again!\");</script>";
+		header("Location: ../html/Login.html");
+	}
+?>
