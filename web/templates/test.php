a083987ff8067eaca207416e948eeaf8bff22def
James <houstonj1@wit.edu>
2017-03-27 15:36:57 -0400

    Updated file structure and naming to better fit Flask. Got rid of outdated python and templates.

--- web/templates/test.php
+++
@@ -1,7 +0,0 @@
-<?php
-  $output=shell_exec("sudo python /home/jahousto/ContainerOrchestrator/bin/list_images.py");
-  $result = json_decode($output,true);
-  foreach($result as $key => $value) {
-    echo "$key   :   $value</br>";
-  }
-?>
