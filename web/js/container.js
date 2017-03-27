ba22733518e8afe6244634d8fbd979bb6539e951
James <houstonj1@wit.edu>
2017-03-27 16:40:13 -0400

    Seperated files into css and js to make things way easier and more readable

---
+++ web/js/container.js
@@ -0,0 +1,64 @@
+var arrButtons = [{"Display_Text":"Home","URL":"Home.html",},{"Display_Text":"Image","URL":"Image.html"},{"Display_Text":"Container","URL":"Container.html"}];
+var imageSource = [{"Display_Text":"Home", "src":"<i class='fa fa-home' aria-hidden='true' style='font-size:35px'></i>"},{"Display_Text":"Image","src":"<i class='fa fa-picture-o' aria-hidden='true' style='font-size:35px'></i>"},{"Display_Text":"Container","src":"<i class='fa fa-sellsy' aria-hidden='true' style='font-size:35px'></i>"}]
+var containerSource =[{"Display_Text":"Create"},{"Display_Text":"Start"},{"Display_Text":"Stop"},{"Display_Text":"Remove"}];
+$(document).ready(function() {
+  var tbl = "<table>";
+  var tr = "<tr>";
+  for (var i=0;i<arrButtons.length;i++) {
+    var url = arrButtons[i].URL;
+    //alert(url);
+    //var td = "<td style='padding:4px;'><input type='button' value='" + arrButtons[i].Display_Text + "' onclick=goto('" +url + "');  /></td>"  ;
+    var td = "<td style='padding:4px;'><input class='menu_buttons textbox' type='button' value='" + arrButtons[i].Display_Text + "'  /><span style='display:none;'>" +url + "</span> </td>"  ;
+    //var myDiv = "<div style='background-color:" + color + "'>" + url  + "</div>"
+    tr = tr + td;
+  }
+  tr= tr + "</tr>";
+  tbl = tbl +  tr + "</table>";
+  $("#dvMenuBar").html(tbl);
+  tbl = "";
+  tr = "";
+  tbl = "<table>";
+  tr = "<tr>";
+  for(var j=0;j<containerSource.length;j++) {
+    var td = "<td style='padding:4px;'><input class='containerBtn containerBtns' type='button' value='" + containerSource[j].Display_Text + "'  /></td>" ;
+    tr = tr + td;
+    //alert(containerSource[j].Display_Text);
+  }
+  tr = tr + "</tr>";
+  tbl = tbl + tr + "</table>";
+  $("#container_menu").html(tbl);
+});
+
+$(document).on("mouseover",".menu_buttons",function() {
+  var btn_offset = $(this).offset();
+  //alert(btn_offset.left);
+  //alert(btn_offset.top);
+  var src;
+  var imgKey = $(this).val();
+  for(var i=0;i<imageSource.length;i++) {
+    //alert(imageSource[i].Display_Text);
+    if(imgKey == imageSource[i].Display_Text) {
+      src = imageSource[i].src;
+      //alert(src);
+      break;
+    }
+  }
+  $("#menu_img_icon").parent().css({position:'relative'});
+  $("#menu_img_icon").css({top: (btn_offset.top)- (1.4*btn_offset.top), left: btn_offset.left + 80, position:'absolute'});
+  //var img = "<img class='img_style' src='" + src + "' ></img>";
+  var img = src
+  $("#menu_img_icon").html(img);
+  //$("#dvMenuBar").append(img);
+})
+$(document).on("mouseleave",".menu_buttons",function() {
+  $("#menu_img_icon").html("");
+})
+function goto(url) {
+  window.location.href = url;
+//alert(url);
+}
+$(document).on("click", ".menu_buttons", function() {
+  var $td = $(this).parent("td");
+  var $span = $td.children("span");
+  window.location.href = $span.text();
+})
---
+++ web/js/home.js
@@ -0,0 +1,58 @@
+var arrButtons = [{"Display_Text":"Home","URL":"Home.html",},{"Display_Text":"Image","URL":"Image.html"},{"Display_Text":"Container","URL":"Container.html"}];
+var imageSource = [{"Display_Text":"Home", "src":"<i class='fa fa-home' aria-hidden='true' style='font-size:35px'></i>"},{"Display_Text":"Image","src":"<i class='fa fa-picture-o' aria-hidden='true' style='font-size:35px'></i>"},{"Display_Text":"Container","src":"<i class='fa fa-sellsy' aria-hidden='true' style='font-size:35px'></i>"}]
+$(document).ready(function() {
+  var tbl = "<table>";
+  var tr = "<tr>";
+  for (var i=0;i<arrButtons.length;i++) {
+    var url = arrButtons[i].URL;
+    var color = 'red';
+    if (i == 0) {
+      color = 'blue';
+    }
+    //alert(url);
+    //var td = "<td style='padding:4px;'><input type='button' value='" + arrButtons[i].Display_Text + "' onclick=goto('" +url + "');  /></td>"  ;
+    var td = "<td style='padding:4px;'><input class='menu_buttons textbox' type='button' value='" + arrButtons[i].Display_Text + "'  /><span style='display:none;'>" +url + "</span> </td>"  ;
+    //var myDiv = "<div style='background-color:" + color + "'>" + url  + "</div>"
+    tr = tr + td;
+  }
+  tr= tr + "</tr>";
+  tbl = tbl +  tr + "</table>";
+  $("#dvMenuBar").html(tbl);
+});
+
+$(document).on("mouseover",".menu_buttons",function() {
+  var btn_offset = $(this).offset();
+  //alert(btn_offset.left);
+  //alert(btn_offset.top);
+  var src;
+  var imgKey = $(this).val();
+  for(var i=0;i<imageSource.length;i++) {
+    //alert(imageSource[i].Display_Text);
+    if(imgKey == imageSource[i].Display_Text) {
+      src = imageSource[i].src;
+      //alert(src);
+      break;
+    }
+  }
+  $("#menu_img_icon").parent().css({position:'relative'});
+  $("#menu_img_icon").css({top: (btn_offset.top)- (1.7*btn_offset.top), left: btn_offset.left + 80, position:'absolute'});
+  //var img = "<img class='img_style' src='" + src + "' ></img>";
+  var img = src
+  $("#menu_img_icon").html(img);
+  //$("#dvMenuBar").append(img);
+})
+
+$(document).on("mouseleave",".menu_buttons",function() {
+  $("#menu_img_icon").html("");
+})
+
+function goto(url) {
+  window.location.href = url;
+  //alert(url);
+}
+
+$(document).on("click", ".menu_buttons", function() {
+  var $td = $(this).parent("td");
+  var $span = $td.children("span");
+  window.location.href = $span.text();
+})
