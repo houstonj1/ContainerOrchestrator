9b8fdc8ebde0fbb83be01b5454db91f08f09a20f
houstonj1 <houstonj1@wit.edu>
2017-08-25 22:11:32 -0400

    Removed some unnecessary files

--- bin/dockerExceptionTest.py
+++
@@ -1,9 +0,0 @@
-import sys
-import docker
-
-client = docker.from_env()
-
-try:
-    client.images.list()
-except ConnectionError as e:
-    print e
--- php/connect.php
+++
@@ -1,6 +0,0 @@
- <?php
-    define('DBHOST', 'localhost');
-    define('DBNAME', 'ContainerOrchestrator');
-    define('DBUSER', 'root');
-    define('DBPASS', 'container');
-?>
